10baaa23b65358fe261eac93210796ec5d3de9fc
tamoto.kyosuke <tamoto.kyosuke@a-tm.co.jp>
2023-12-29 12:58:55 +0900

    update: mini-css-extract-plugin css-minimizer-plugin install

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,33 @@
 const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
 
 module.exports = {
   entry: './app/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'), 
     filename: 'my-bundle.js',
+  },
+
+  plugins: [
+    new MiniCssExtractPlugin({
+    filename: './css/index.css',
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      }
+    ]
+  },
+
+  optimization: {
+    //minimizaer: true,
+    minimizer: [
+      new CssMinimizerPlugin(),
+    ]
   }
 };

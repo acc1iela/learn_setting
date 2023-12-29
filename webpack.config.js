211b91f21f8b50b9a01a3204fb614bb9a1fae87b
tamoto.kyosuke <tamoto.kyosuke@a-tm.co.jp>
2023-12-29 12:33:50 +0900

    update&add: index.js追加 インデント修正

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,8 @@ const path = require('path')
 
 module.exports = {
   entry: './app/index.js',
-
-output: {
-path: path.resolve(__dirname, 'dist'), 
-filename: 'my-bundle.js',
-
+  output: {
+    path: path.resolve(__dirname, 'dist'), 
+    filename: 'my-bundle.js',
   }
-
 };
